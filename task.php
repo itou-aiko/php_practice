a39dcd8b993a8eba76b8a7abad4434aba56d409f
itou <aikodon0726@icloud.com>
2024-11-13 07:44:57 +0000

    課題

--- task.php
+++ task.php
@@ -1,4 +1,29 @@
 <?php
+
+//課題１
+$a = 3;
+$b = 7;
+echo $a + $b . PHP_EOL;
+
+//課題２
+$array_month = [
+'1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'
+];
+echo $array_month[7] . PHP_EOL;
+
+//課題３
+$hello = "Hello,";
+$name = "aiko";
+$world = "'s World!";
+
+echo $hello . $name . $world . PHP_EOL;
+
+//課題４
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost . PHP_EOL;
+
+//課題５
 $calendar_2018 = [
     "January" => "1月",
     "February" => "2月",
